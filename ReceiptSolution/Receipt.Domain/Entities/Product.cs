5a2005336772db02bd43da915eae2e0a87c89bc1
Jacek Arciszewski <arciszewski.jm@gmail.com>
2016-10-19 15:43:13 +0200

    Added simple Product model.

---
+++ ReceiptSolution/Receipt.Domain/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Receipt.Domain.Entities
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string purchasePlace { get; set; }
+        public DateTime purchaseDate { get; set; }
+        public DateTime addDate { get; set; }
+   
+        public Product(string name, string category, string purchasePlace, DateTime purchaseDate)
+        {
+            this.Name = name;
+            this.Category = category;
+            this.purchasePlace = purchasePlace;
+            this.purchaseDate = purchaseDate;
+        }
+    }
+}
