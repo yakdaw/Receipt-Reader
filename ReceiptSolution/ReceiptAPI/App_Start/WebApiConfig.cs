4b729d12ac64d2ca5a29702851c03222ba865d53
Jacek Arciszewski <arciszewski.jm@gmail.com>
2016-10-20 20:16:05 +0200

    Provided Autofac DI sample.

--- ReceiptSolution/ReceiptAPI/App_Start/WebApiConfig.cs
+++ ReceiptSolution/ReceiptAPI/App_Start/WebApiConfig.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
+using Autofac;
+using Receipt.API.Model.EF;
+using Receipt.API.Model;
+using Autofac.Integration.WebApi;
+using System.Reflection;
+
 namespace Receipt.API
 {
     public static class WebApiConfig
@@ -10,6 +16,13 @@ namespace Receipt.API
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<ProductRepository>().As<IProductRepository>()
+                .InstancePerRequest();
+
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
