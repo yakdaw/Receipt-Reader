931dd7cc37a83e3e938f6340771a897665a08e16
Jacek Arciszewski <arciszewski.jm@gmail.com>
2016-10-25 17:33:10 +0200

    Removed confirm password from User.

--- ReceiptSolution/Receipt.API/Authentication/User.cs
+++ ReceiptSolution/Receipt.API/Authentication/User.cs
@@ -13,10 +13,5 @@
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
     }
 }
