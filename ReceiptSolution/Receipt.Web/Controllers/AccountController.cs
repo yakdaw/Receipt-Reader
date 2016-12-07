2ddc4d1806674910dca00637f171897aa9f3bd46
Jacek Arciszewski <arciszewski.jm@gmail.com>
2016-12-07 19:29:45 +0100

    Configured bootstrap. Added default layout and logic behind css.

---
+++ ReceiptSolution/Receipt.Web/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿namespace Receipt.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    [RoutePrefix("account")]
+    public class AccountController : Controller
+    {
+        [Route("login")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("register")]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [Route("lostpassword")]
+        public ActionResult LostPassword()
+        {
+            return View();
+        }
+    }
+}
--- ReceiptSolution/Receipt.Web/Controllers/HomeController.cs
+++ ReceiptSolution/Receipt.Web/Controllers/HomeController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Receipt.Web.Controllers
+﻿namespace Receipt.Web.Controllers
 {
+    using System.Web.Mvc;
+
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
