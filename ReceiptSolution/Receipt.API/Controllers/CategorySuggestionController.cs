575487bc5e9ab5e58feee3364fd75d72828330de
Jacek Arciszewski <arciszewski.jm@gmail.com>
2016-12-27 00:05:28 +0100

    Supported multiple category suggestions. Added more logic to suggestion controller.

--- ReceiptSolution/Receipt.API/Controllers/CategorySuggestionController.cs
+++ ReceiptSolution/Receipt.API/Controllers/CategorySuggestionController.cs
@@ -3,6 +3,7 @@
     using Model;
     using Models;
     using Services;
+    using System.Collections.Generic;
     using System.Net;
     using System.Net.Http;
     using System.Web.Http;
@@ -11,7 +12,7 @@
     [Authorize]
     public class CategorySuggestionController : ApiController
     {
-        private readonly ISuggestionService suggestionService;
+        private ISuggestionService suggestionService;
         private readonly AuthService authService;
 
         public CategorySuggestionController(ISuggestionService suggestionService)
@@ -20,11 +21,24 @@
             this.authService = new AuthService();
         }
 
+        /// <summary>
+        /// Suggest category for product
+        /// </summary>
+        /// <param name="userName">User name</param>
+        /// <param name="categorySuggestion">Product name and purchase place</param>
+        /// <response code="200">Successfully suggested category ID.</response>
+        /// <response code="400">Wrong JSON request model.</response>
+        /// <response code="401">No authentication token. / Wrong user name in query.</response>
         [HttpPost]
         [ResponseType(typeof(int))]
         [Route("api/{userName}/category/suggest")]
         public HttpResponseMessage SuggestCategory(string userName, CategorySuggestionModel categorySuggestion)
         {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Bad request model");
+            }
+
             string tokenName = this.authService.GetUserName(this.User);
 
             if (!tokenName.Equals(userName))
@@ -38,6 +52,46 @@
                 (userId, categorySuggestion.ProductName, categorySuggestion.PurchasePlace);
 
             return Request.CreateResponse(HttpStatusCode.OK, categoryId);
+        }
+
+        /// <summary>
+        /// Suggest categories for products list
+        /// </summary>
+        /// <param name="userName">User name</param>
+        /// <param name="categorySuggestions">Products names and purchase places</param>
+        /// <response code="200">Successfully suggested categories IDs.</response>
+        /// <response code="400">Wrong JSON request model.</response>
+        /// <response code="401">No authentication token. / Wrong user name in query.</response>
+        [HttpPost]
+        [ResponseType(typeof(IEnumerable<int>))]
+        [Route("api/{userName}/category/suggestMultiple")]
+        public HttpResponseMessage SuggestCategories(string userName, IEnumerable<CategorySuggestionModel> categorySuggestions)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Bad request model");
+            }
+
+            string tokenName = this.authService.GetUserName(this.User);
+
+            if (!tokenName.Equals(userName))
+            {
+                return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            }
+
+            string userId = this.authService.GetUserId(this.User);
+
+            var categoryIds = new List<int>();
+
+            foreach (var categorySuggestion in categorySuggestions)
+            {
+                var categoryId = suggestionService.SuggestProductCategoryId
+                    (userId, categorySuggestion.ProductName, categorySuggestion.PurchasePlace);
+
+                categoryIds.Add(categoryId);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, categoryIds);
         }
     }
 }
