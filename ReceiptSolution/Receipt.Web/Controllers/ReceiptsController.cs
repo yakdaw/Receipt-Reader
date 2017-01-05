640992a3cfadc557f7d3ce4bc7017f1c35f9d14a
Jacek Arciszewski <arciszewski.jm@gmail.com>
2017-01-05 15:44:19 +0100

    Added receipt controller.

---
+++ ReceiptSolution/Receipt.Web/Controllers/ReceiptsController.cs
@@ -0,0 +1,45 @@
+﻿namespace Receipt.Web.Controllers
+{
+    using RestSharp;
+    using Services;
+    using System.Collections.Generic;
+    using System.Web.Configuration;
+    using System.Web.Mvc;
+    using ViewModels;
+
+    [RoutePrefix("receipts")]
+    public class ReceiptsController : Controller
+    {
+        readonly AuthorizationService authorizationService;
+
+        public ReceiptsController()
+        {
+            authorizationService = new AuthorizationService();
+        }
+
+        [HttpGet]
+        [Route("", Name = "Receipts")]
+        public ActionResult GetAllUserReceipts()
+        {
+            var client = new RestClient(WebConfigurationManager.AppSettings["webApiUrl"]);
+            var request = authorizationService.GenerateAuthorizedRequest("/receipts/", Method.GET, HttpContext);
+
+            var response = client.Execute<List<ReceiptModel>>(request).Data;
+
+            return View(response);
+        }
+
+        [HttpGet]
+        [Route("{receiptId}", Name = "ReceiptProducts")]
+        public ActionResult GetUserReceiptProducts(int receiptId)
+        {
+            var client = new RestClient(WebConfigurationManager.AppSettings["webApiUrl"]);
+            var request = authorizationService
+                .GenerateAuthorizedRequest("/receipts/" + receiptId + "/products/", Method.GET, HttpContext);
+
+            var response = client.Execute<List<ProductModel>>(request).Data;
+
+            return View(response);
+        }
+    }
+}
