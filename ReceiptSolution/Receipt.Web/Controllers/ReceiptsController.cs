e940be1ed445ccb519d85ccdc3de9d54d59e7f91
Jacek Arciszewski <arciszewski.jm@gmail.com>
2017-01-08 16:47:22 +0100

    Implemented almost everything else.

--- ReceiptSolution/Receipt.Web/Controllers/ReceiptsController.cs
+++ ReceiptSolution/Receipt.Web/Controllers/ReceiptsController.cs
@@ -1,8 +1,11 @@
 ﻿namespace Receipt.Web.Controllers
 {
+    using Models;
     using RestSharp;
     using Services;
+    using System;
     using System.Collections.Generic;
+    using System.Net;
     using System.Web.Configuration;
     using System.Web.Mvc;
     using ViewModels;
@@ -30,7 +33,7 @@
         }
 
         [HttpGet]
-        [Route("{receiptId}", Name = "ReceiptProducts")]
+        [Route("{receiptId}/products", Name = "ReceiptProducts")]
         public ActionResult GetUserReceiptProducts(int receiptId)
         {
             var client = new RestClient(WebConfigurationManager.AppSettings["webApiUrl"]);
@@ -40,6 +43,191 @@
             var response = client.Execute<List<ProductModel>>(request).Data;
 
             return View(response);
+        }
+
+        [HttpGet]
+        [Route("{receiptId}/update", Name = "UpdateReceipt")]
+        public ActionResult UpdateReceipt(int receiptId)
+        {
+            var client = new RestClient(WebConfigurationManager.AppSettings["webApiUrl"]);
+            var request = authorizationService.GenerateAuthorizedRequest
+                ("/receipts/" + receiptId, Method.GET, HttpContext);
+
+            var receiptModel = client.Execute<ReceiptModel>(request).Data;
+
+            var receiptUpdateModel = new ReceiptUpdateModel();
+            receiptUpdateModel.MapFromApiModel(receiptModel);
+
+            return View(receiptUpdateModel);
+        }
+
+        [HttpPost]
+        [Route("{receiptId}/update", Name = "SendUpdatedReceipt")]
+        public ActionResult UpdateReceipt(int receiptId, ReceiptUpdateModel receiptUpdateModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new RestClient(WebConfigurationManager.AppSettings["webApiUrl"]);
+                var request = authorizationService.GenerateAuthorizedRequest
+                    ("/receipts/" + receiptId + "/", Method.PUT, HttpContext);
+
+                request.RequestFormat = DataFormat.Json;
+
+                // Temporary fix date due to AddJsonBody issue
+                if (receiptUpdateModel.PurchaseDate != null)
+                {
+                    var offset = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow);
+                    receiptUpdateModel.PurchaseDate += offset;
+                }
+                request.AddJsonBody(receiptUpdateModel);
+
+                var response = client.Execute(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return RedirectToAction("");
+                }
+                else
+                {
+                    ViewBag.WrongMessage = response.ErrorMessage;
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("{receiptId}", Name = "DeleteReceipt")]
+        public ActionResult DeleteReceipt(int receiptId)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new RestClient(WebConfigurationManager.AppSettings["webApiUrl"]);
+                var request = authorizationService.GenerateAuthorizedRequest
+                    ("/receipts/" + receiptId + "/", Method.DELETE, HttpContext);
+
+                var response = client.Execute(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return RedirectToAction("");
+                }
+                else
+                {
+                    ViewBag.WrongMessage = response.ErrorMessage;
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("{receiptId}/newproduct", Name = "NewProduct")]
+        public ActionResult AddNewProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("{receiptId}/newproduct", Name = "PostNewProduct")]
+        public ActionResult AddNewProduct(int receiptId, NewProductModel newProductModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new RestClient(WebConfigurationManager.AppSettings["webApiUrl"]);
+                var request = authorizationService.GenerateAuthorizedRequest
+                    ("/receipts/" + receiptId + "/products/", Method.POST, HttpContext);
+
+                request.RequestFormat = DataFormat.Json;
+                request.AddJsonBody(newProductModel);
+
+                var response = client.Execute(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return RedirectToAction("GetUserReceiptProducts");
+                }
+                else
+                {
+                    ViewBag.WrongMessage = response.ErrorMessage;
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("{receiptId}/products/{productId}/update", Name = "UpdateProduct")]
+        public ActionResult UpdateProduct(int receiptId, int productId)
+        {
+            var client = new RestClient(WebConfigurationManager.AppSettings["webApiUrl"]);
+            var request = authorizationService.GenerateAuthorizedRequest
+                ("/receipts/" + receiptId + "/products/" + productId, Method.GET, HttpContext);
+
+            var productModel = client.Execute<ProductModel>(request).Data;
+
+            var productUpdateModel = new ProductUpdateModel();
+            productUpdateModel.MapFromApiModel(productModel);
+
+            return View(productUpdateModel);
+        }
+
+        [HttpPost]
+        [Route("{receiptId}/products/{productId}/update", Name = "SendUpdatedProduct")]
+        public ActionResult UpdateProduct(int receiptId, int productId, ProductUpdateModel productUpdateModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new RestClient(WebConfigurationManager.AppSettings["webApiUrl"]);
+                var request = authorizationService.GenerateAuthorizedRequest
+                    ("/receipts/" + receiptId + "/products/" + productId + "/", Method.PUT, HttpContext);
+
+                request.RequestFormat = DataFormat.Json;
+                request.AddJsonBody(productUpdateModel);
+
+                var response = client.Execute(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return RedirectToAction("GetUserReceiptProducts", new { receiptId = receiptId });
+                }
+                else
+                {
+                    ViewBag.WrongMessage = response.ErrorMessage;
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("{receiptId}/products/{productId}", Name = "DeleteProduct")]
+        public ActionResult DeleteProduct(int receiptId, int productId)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new RestClient(WebConfigurationManager.AppSettings["webApiUrl"]);
+                var request = authorizationService.GenerateAuthorizedRequest
+                    ("/receipts/" + receiptId + "/products/" + productId + "/", Method.DELETE, HttpContext);
+
+                var response = client.Execute(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return RedirectToAction("GetUserReceiptProducts", new { receiptId = receiptId });
+                }
+                else
+                {
+                    ViewBag.WrongMessage = response.ErrorMessage;
+                    return View();
+                }
+            }
+
+            return View();
         }
     }
 }
---
+++ ReceiptSolution/Receipt.Web/Models/ReceiptUpdateModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Receipt.Web.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using ViewModels;
+
+    public class ReceiptUpdateModel
+    {
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        [Display(Name = "Purchase date")]
+        public DateTime? PurchaseDate { get; set; }
+
+        [Display(Name = "Purchase place")]
+        public string PurchasePlace { get; set; }
+
+        public void MapFromApiModel(ReceiptModel apiModel)
+        {
+            this.PurchaseDate = apiModel.PurchaseDate;
+            this.PurchasePlace = apiModel.PurchasePlace;
+        }
+    }
+}
